18761524ad3d4c8e638c5bbb43994ca4c0acd747
Ashokaas <antotosun@gmail.com>
2022-09-23 01:21:49 +0200

    oui

--- projet_solitaire/main.py
+++ projet_solitaire/main.py
@@ -1,33 +1,76 @@
+# =====================================
+#      IMPORTATIONS DES LIBRAIRIES 
+# =====================================
+
+# Tkinter
+from tkinter import *
+from tkinter import ttk, filedialog
+from PIL import ImageTk, Image
+
+# Necessaires au bon fonctionnement d'un jeu de carte
 import Carte
 import JeuCarte
 import pile as p
+
+# Supplémentaires
 import random
 from colorama import Fore
 import os
 
 
-"""
-Class game
-"""
 
 # Fonction pour clear la console
 clear = lambda:os.system('clear')
 
+
+
+# class Root
+class Root:
+    def __init__(self):
+                # -- Configuration de la fenêtre
+        self.root = Tk()
+        self.root.title("Solitaire v3")
+
+        self.root.resizable(False, False)
+
+        hauteur = 600
+        largeur = 850
+        self.root.geometry(str(largeur) + 'x' + str(hauteur))
+
+        # -- Frame Talon
+        self.frame_talon = Frame(self.root, bg="black", width=200, height=200)
+        self.frame_talon.pack(expand=0, fill=Y, side=LEFT)
+        self.frame_talon.pack_propagate(False)
+        
+        self.label_talon = Label(self.frame_talon, text="Talon")
+        self.label_talon.grid(column=0, row=0)
+        self.label_talon.pack_propagate(False)
+        
+
+
+
+# class Game
 class Game:
     
     def __init__(self, nb_cartes:str):
+        # Asssertion pour le nombre de cartes
         assert nb_cartes in ["32", "52"], "Le nombre de cartes doit être égal à 32 ou 52"
+        
+        # Définition du jeu en fonction du nombre de cartes
         self.jeu_cartes = JeuCarte.JeuCarte(nb_cartes)
         self.jeu_cartes.melangerJeu()
         
+        # Définition du talon et de la main en cours
         self.talon = p.Pile(self.jeu_cartes.getJeu())
-        self.talon_pioche = p.Pile()
+        self.talon_main = p.Pile()
         
+        # Définition des 4 défausses
         self.defausse1 = p.Pile()
         self.defausse2 = p.Pile()
         self.defausse3 = p.Pile()
         self.defausse4 = p.Pile()
         
+        # Définition des 4 types
         self.piques = p.Pile()
         self.carreaux = p.Pile()
         self.trefles = p.Pile()
@@ -41,6 +84,8 @@ class Game:
         
         
 
+
+fenetre = Root()
 # Clear la console
 clear()
 
@@ -64,5 +109,3 @@ else:
     
     
     
-while True:
-    pass
